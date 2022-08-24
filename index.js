cc8d77e71effca376ed07ceca08525259a0026ff
thadeus <thadeusnyariki94@gmail.com>
2022-08-24 11:09:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+const data = {name: 'Steve', email: 'steve@steve.com'};
+
+function submitData(){
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+   .then(response => response.json())
+   .then(data => {
+    console.log('Success:', data);
+   })
+   .catch(error => {
+    console.error('Error:', error)
+   })
+}
+
